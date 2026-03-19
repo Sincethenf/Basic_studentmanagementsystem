3fe133555525a658df4da4209add3a8a2bd1d9fa
Ren <seneswarren@gmail.com>
2026-03-20 06:11:32 +0800

    initial commit

---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+include './config/connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Form</title>
+</head>
+<body>
+   <div class="container">
+      <form method="POST">
+         
+      </form>
+   </div>
+</body>
+</html>
