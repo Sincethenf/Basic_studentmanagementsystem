365fbfc9949b8ebb3539e29a5cbe9248dfc37d87
Ren <seneswarren@gmail.com>
2026-03-20 06:27:52 +0800

    initial user.php

--- user.php
+++ user.php
@@ -1,5 +1,18 @@
 <?php
 include './config/connect.php';
+if(isset($_POST['submit'])){
+   $name = $_POST['name'];
+   $age = $_POST['age'];
+   $course = $_POST['course'];
+
+   $sql = "INSERT INTO student(name, age, course) VALUES ('$name','$age','$course')";
+   $result = mysqli_query($con, $sql);
+
+   if($result){
+      echo "<script>alert('successfuly inputed')</script>";
+      echo "<script>window.open(_self)</script>";
+   }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
